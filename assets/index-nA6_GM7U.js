9dfef7cd60ccfe0de3df3c6714218e357d5f4683
David Mendoza <dmendozasommer@gmail.com>
2024-02-08 19:37:13 +0100

    Updates

--- assets/index-nA6_GM7U.js
+++ assets/index-nA6_GM7U.js
@@ -3,13 +3,20 @@
     <img src="/assets/logo_no_bg.png" id="logo" alt="Logo" />
     <p id="logo_title">Colegio </br> Jesús de </br> Nazareth</p>
   </a>
-  <div id="menu">
+  <section id="menu">
     <a class="nav_link" href="#history_container">Historia</a>
     <a class="nav_link" href="#mision_container">Misión y Visión</a>
     <a class="nav_link" href="#news_container">Noticias</a>
-    <div id="btn_close_menu"></div>
+    <div id="btn_close_menu">
+      <span class="material-symbols-outlined">
+        close_small
+      </span>
+    </div>
+    <img src="/assets/rainbow.png" id="rainbow" alt="rainbow" />
+  </section>
+  <div id="btn_menu">
+    <span class="material-symbols-outlined">menu</span>
   </div>
-  <div id="btn_menu"></div>
 </nav>`}document.querySelector("#app").innerHTML=`
   <div id="background">
     <div class="circle" id="circle_top"></div>
