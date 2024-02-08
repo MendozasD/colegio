5e0cf7a4b14cc090a343e09c73e0327b881370f7
David Mendoza <dmendozasommer@gmail.com>
2024-02-08 19:37:03 +0100

    Rainbow added

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -4,12 +4,19 @@ export function navbar(a) {
     <img src="/assets/logo_no_bg.png" id="logo" alt="Logo" />
     <p id="logo_title">Colegio </br> Jesús de </br> Nazareth</p>
   </a>
-  <div id="menu">
+  <section id="menu">
     <a class="nav_link" href="#history_container">Historia</a>
     <a class="nav_link" href="#mision_container">Misión y Visión</a>
     <a class="nav_link" href="#news_container">Noticias</a>
-    <div id="btn_close_menu"></div>
+    <div id="btn_close_menu">
+      <span class="material-symbols-outlined">
+        close_small
+      </span>
+    </div>
+    <img src="/assets/rainbow.png" id="rainbow" alt="rainbow" />
+  </section>
+  <div id="btn_menu">
+    <span class="material-symbols-outlined">menu</span>
   </div>
-  <div id="btn_menu"></div>
 </nav>`;
 }
